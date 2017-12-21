dab517f67caf12f689d1430b79a4c29c9550138e
xiaoming.wang <18600882246@163.com>
2017-12-21 10:41:36 +0800

    add offset auto

---
+++ kafka-example/src/main/java/com/github/example/kafka/conusmer/OffsetAutoSubmitExecuter.java
@@ -0,0 +1,37 @@
+package com.github.example.kafka.conusmer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ *  自动提交offset到zk
+ *
+ * @author Michael.Wang
+ * @date 2017/12/21
+ */
+public class OffsetAutoSubmitExecuter {
+    public static final String URL = "localhost:9093";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", URL);
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("my-replicated-topic", "bar"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
